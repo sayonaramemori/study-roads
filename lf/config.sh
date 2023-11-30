1a3a54e75a24ff4c19df3dc4d5ef149a3a184b8f
memoris <sayonara@1342733420@qq.com>
2023-11-30 21:21:19 +0800

    fix

--- lf/config.sh
+++ lf/config.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo mkdir /etc/lf -p
-sudo cp ./lfrc /etc/lf/
-sudo cat ./lfcd >> ~/.bashrc
+if [ -e ./lf ]; then
+    sudo mkdir /etc/lf -p
+    sudo cp ./lfrc /etc/lf/
+    sudo cat ./lfcd >> ~/.bashrc
+    source ~/.bashrc
+    echo "Config OK, use ra to run lfcd"
+else
+    echo "Please download lf first"
+fi
