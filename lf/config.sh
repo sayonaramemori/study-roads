b61c79ca46cfe97f2284d21d0678a9b7111d67fe
memoris <sayonara@1342733420@qq.com>
2023-12-01 08:53:29 +0800

    fix

--- lf/config.sh
+++ lf/config.sh
@@ -3,8 +3,9 @@ if [ -e ./lf ]; then
     sudo mkdir /etc/lf -p
     sudo cp ./lfrc /etc/lf/
     sudo cat ./lfcd >> ~/.bashrc
+    sudo cp ./lf /usr/bin/
     source ~/.bashrc
-    echo "Config OK, use ra to run lfcd"
+    echo "Config OK, use ra to run lfcd, use q to return to the terminal"
 else
-    echo "Please download lf first"
+    echo "Please download lf binary file first"
 fi
